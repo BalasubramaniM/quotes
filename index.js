010086cb13277f623aab759a003760015d148057
Balasubramani M <bala.m@live.in>
2018-09-25 22:48:51 +0530

    Disabling service worker temporarily.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 window.addEventListener("load", async e => {
 	await fetchQuotes();
 
-	if ("serviceWorker" in navigator) {
-		try {
-			navigator.serviceWorker.register("service-worker.js");
-			console.log("Service Worker registered.");
-		} catch (error) {
-			console.log("Service Worker failed");
-		}
-	}
+	// if ("serviceWorker" in navigator) {
+	// 	try {
+	// 		navigator.serviceWorker.register("service-worker.js");
+	// 		console.log("Service Worker registered.");
+	// 	} catch (error) {
+	// 		console.log("Service Worker failed");
+	// 	}
+	// }
 });
